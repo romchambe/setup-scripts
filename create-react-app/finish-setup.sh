082d5528467d44106fcd24f0ae22ac2cfbe5ba10
romchambe <chamberomain@gmail.com>
2020-11-18 11:06:05 +0100

    move defaults to a dedicated folder and add target dir cmds

--- create-react-app/finish-setup.sh
+++ create-react-app/finish-setup.sh
@@ -8,11 +8,19 @@ echo "Setting up ${TARGET_DIR}"
 
 if [[ -d $TARGET_DIR ]]
 then 
-  cp ${COMMAND_DIR}/.eslintrc.js $TARGET_DIR
-  cp ${COMMAND_DIR}/tailwind.config.js $TARGET_DIR
+  cp ${COMMAND_DIR}/defaults/.eslintrc.js $TARGET_DIR
+  cp ${COMMAND_DIR}/defaults/tailwind.config.js $TARGET_DIR
+  cp ${COMMAND_DIR}/defaults/.prettierrc $TARGET_DIR
 
   cd $TARGET_DIR
-  
+
+  mkdir src/assets
+  mkdir src/assets/styles
+  mv src/index.css src/assets/styles
+  cp ${COMMAND_DIR}/defaults/tailwind.css src/assets/styles
+
+  yarn add -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-jest
+  yarn add -D prettier eslint-config-prettier eslint-plugin-prettier
 else
   echo "Cannot find target directory" 1>&2
   exit 1
